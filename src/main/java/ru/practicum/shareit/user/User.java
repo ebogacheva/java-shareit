88e528c828e04c94a25e2673005a31333f6d47c9
Ekaterina Bogacheva <bogacheva.e@gmail.com>
2023-05-20 23:54:23 +0300

    feat: созданны классы объектов-сущностей

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.Email;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
+
+    private Long id;
+    private String name;
+    @Email
+    @EqualsAndHashCode.Include
+    private String email;
 }
