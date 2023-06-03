13d0cb851e66500ed6ad6c9033539e35163c9211
Ekaterina Bogacheva <bogacheva.e@gmail.com>
2023-06-04 01:58:05 +0300

    feat: schema sql, jpa-entities with associations

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,20 +1,35 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
 
 /**
  * TODO Sprint add-controllers.
  */
-@Data
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder(toBuilder = true)
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "is_available")
     private boolean available;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private User owner;
+    @OneToOne
+    @JoinColumn(name = "request_id")
     private ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,8 +1,8 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -10,10 +10,18 @@ import javax.validation.constraints.NotNull;
 /**
  * TODO Sprint add-controllers.
  */
-@Data
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder(toBuilder = true)
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name")
     private String name;
     @Email
     @NotNull
