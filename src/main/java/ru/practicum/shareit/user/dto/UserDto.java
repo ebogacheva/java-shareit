490c7c0168e31c94d2eb3e630477d76ac269e0b0
Ekaterina Bogacheva <bogacheva.e@gmail.com>
2023-06-26 00:14:31 +0300

    test: add UserServiceImplTest

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
@@ -8,6 +9,7 @@ import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
+@Builder(toBuilder = true)
 public class UserDto {
 
     private Long id;
